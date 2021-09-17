988aac556acb2924c591ee259b5c5323895cf4c5
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-09-17 15:55:40 +0200

    fixed error on font.js

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  
+  <header>
+    <nav>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Chi siamo</a></li>
+        <li><a href="">Contatti</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
